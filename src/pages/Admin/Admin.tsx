30eb482bc617b566777858120b3eeb409ed60f56
JongHyun Ryu <willowise@naver.com>
2026-01-21 16:30:21 +0900

    feat: admin 페이지 라우팅 및 header UI 구현

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,25 @@
+import logo from '@/assets/images/logo.png';
+import BottomNav from '@/components/BottomNav';
+import * as admin from '../Admin/style/Admin.css';
+import * as styles from '../Home/style/Home.css';
+import Layout from '../layout/Layout';
+
+export default function Admin() {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        {/* 로고 */}
+        <img src={logo} alt="다무너" className={styles.logo} />
+
+        {/* 헤더 */}
+        <div className={admin.header}>
+          <h1>관리자 페이지</h1>
+          <h4>
+            상담 요약 이용 고객 수 <span className={admin.count}>700</span>명
+          </h4>
+        </div>
+      </div>
+      <BottomNav />
+    </Layout>
+  );
+}
